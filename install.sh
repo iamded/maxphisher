34707fc988f59e67dbf536011176a6261f183014
iamded <dedetmasri@gmail.com>
2022-11-01 10:06:05 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+echo "Dont forget subcribe my chanel: iam ded !!!..."
+sleep 3
+termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
+clear
+sleep 5
+echo "Checking package harap menunggu ya bos..!!!"
+sleep 3
+pkg install python bash git -y
+pkg install git python3 php openssh -y
+pip3 install rich bs4 requests
+echo "Packege success install now run tools!!!"
+clear
+sleep 3
+python3 maxphisher.py
